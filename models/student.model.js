e5978616b23bbe43ba47777bbfbb244ef0290f4c
Mohammad Fahd <117509288+fahd-shakeel@users.noreply.github.com>
2024-08-25 00:41:30 +0530

    Update student.model.js

--- models/student.model.js
+++ models/student.model.js
@@ -4,7 +4,8 @@ var Student = mysql.createConnection({
     host:'sql12.freemysqlhosting.net',
     port:'3306',
     user:'sql12727656',
-    password:"ZmmQNthEXW"
+    password:"ZmmQNthEXW",
+    database:"sql12727656"
 })
 
 module.exports = Student
