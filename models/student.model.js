f774244c8a93757779caa126c2af56c6aeab2c42
Mohammad Fahd <117509288+fahd-shakeel@users.noreply.github.com>
2024-08-24 23:31:17 +0530

    Update student.model.js

--- models/student.model.js
+++ models/student.model.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql');
 
 var Student = mysql.createConnection({
-    host:"https://schools-record-managment.onrender.com",
+    host:"127.0.0.1",
+    port:"3306",
     user:"root",
     password:"root",
     database: 'school'
